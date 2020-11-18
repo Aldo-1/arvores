41667c1bb6215035020bfd4992ea4059796a3549
Pedro Santos <pedrobsantos98@outlook.com>
2020-11-18 02:15:47 -0300

    Implementado método de remoção de elementos na arvore avl

--- src/App.java
+++ src/App.java
@@ -2,14 +2,26 @@ import model.AVLTree;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        AVLTree<Integer> avlTree = new AVLTree<>();
-        // avlTree.add(10);
-        // avlTree.add(20);
-
-        // avlTree.add(35);
-        avlTree.add(11);
-        avlTree.add(12);
-        avlTree.add(13);
-
+        AVLTree<Integer> tree = new AVLTree<>();
+        tree.add(10);
+        tree.add(20);
+        tree.add(35);
+        tree.add(50);
+        tree.add(30);
+        tree.add(40);
+        tree.add(41);
+        tree.add(42);
+        tree.add(45);
+        tree.add(60);
+        tree.add(70);
+        tree.add(46);
+        tree.add(80);
+        tree.remove(60);
+        tree.remove(70);
+        tree.remove(50);
+        tree.remove(46);
+        System.out.println("Pre-Order: " + tree.preOrder());
+        System.out.println("Post-Order: " + tree.postOrder());
+        System.out.println("In-Order: " + tree.inOrder());
     }
 }
--- src/model/AVLTree.java
+++ src/model/AVLTree.java
@@ -54,7 +54,110 @@ public class AVLTree<T extends Comparable<T>> extends Tree<T> {
 
     @Override
     public void remove(T element) {
+        this.auxRemove(element, this.root);
+    }
 
+    private void auxRemove(T element, Node<T> root) {
+        if (root != null) {
+            if (root.getElement().compareTo(element) == 0) {
+                Node<T> unbalanced = null;
+                if (root.getLeft() == null && root.getRight() == null) {
+                    if (root == this.root) {
+                        this.root = null;
+                    } else if (root.getFather().getLeft() == root) {
+                        root.getFather().setLeft(null);
+                    } else {
+                        root.getFather().setRight(null);
+                    }
+                    unbalanced = root.getFather();
+                } else if (root.getLeft() != null && root.getRight() == null) {
+                    root.getLeft().setFather(root.getFather());
+                    if (root == this.root) {
+                        this.root = root.getLeft();
+                    } else if (root.getFather().getLeft() == root) {
+                        root.getFather().setLeft(root.getLeft());
+                    } else {
+                        root.getFather().setRight(root.getLeft());
+                    }
+                    unbalanced = root.getLeft();
+                } else if (root.getLeft() == null && root.getRight() != null) {
+                    root.getRight().setFather(root.getFather());
+                    if (root == this.root) {
+                        this.root = root.getRight();
+                    } else if (root.getFather().getLeft() == root) {
+                        root.getFather().setLeft(root.getRight());
+                    } else {
+                        root.getFather().setRight(root.getRight());
+                    }
+                    unbalanced = root.getRight();
+                } else {
+                    Node<T> successor = this.auxMin(root.getRight());
+                    unbalanced = successor;
+                    if (successor.getFather() != root) {
+                        if (successor.getFather().getLeft() == successor) {
+                            successor.getFather().setLeft(successor.getRight());
+                        } else {
+                            successor.getFather().setRight(successor.getRight());
+                        }
+                        if (successor.getRight() != null) {
+                            successor.getRight().setFather(successor.getFather());
+                        }
+                        successor.setRight(root.getRight());
+                        root.getRight().setFather(successor);
+                        unbalanced = successor.getFather();
+                    }
+                    successor.setFather(root.getFather());
+                    successor.setLeft(root.getLeft());
+                    root.getLeft().setFather(successor);
+                    if (root == this.root) {
+                        this.root = successor;
+                    } else {
+                        if (root.getFather().getLeft() == root) {
+                            root.getFather().setLeft(successor);
+                        } else {
+                            root.getFather().setRight(successor);
+                        }
+                    }
+                }
+                this.balancTreeRemove(unbalanced);
+            } else if (element.compareTo(root.getElement()) == 1) {
+                this.auxRemove(element, root.getRight());
+            } else {
+                this.auxRemove(element, root.getLeft());
+            }
+        }
+    }
+
+    private void balancTreeRemove(Node<T> root) {
+        if (root != null) {
+            this.balancFactor(root);
+            Node<T> left = root.getLeft();
+            Node<T> right = root.getRight();
+            if (root.getBf() == 2) {
+                if (left != null && left.getBf() == 1) {
+                    rightRotation(root);
+                } else if (right != null && right.getBf() == -1) {
+                    leftRotation(right);
+                    rightRotation(root);
+                } else if (root.getHeightLeft() > root.getHeightRight()) {
+                    rightRotation(root);
+                }
+            } else if (root.getBf() == -2) {
+                if (right != null && right.getBf() == -1) {
+                    leftRotation(root);
+                } else if (left != null && left.getBf() == 1) {
+                    rightRotation(left);
+                    leftRotation(root);
+                } else if (root.getHeightLeft() < root.getHeightRight()) {
+                    leftRotation(root);
+                }
+            }
+            this.balancTreeRemove(root.getFather());
+        }
+    }
+
+    private Node<T> auxMin(Node<T> root) {
+        return root.getLeft() == null ? root : this.auxMin(root.getLeft());
     }
 
     private int balancFactor(Node<T> root) {
